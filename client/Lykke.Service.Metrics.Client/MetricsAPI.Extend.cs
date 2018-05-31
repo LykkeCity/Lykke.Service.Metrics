96be150c0bea3ec8655635d67693be3930fb2a5c
Roman Chuprikov <paradise.develop@gmail.com>
2018-05-31 21:30:57 +0500

    prevent memory leak

---
+++ client/Lykke.Service.Metrics.Client/MetricsAPI.Extend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Lykke.Service.Metrics.AutorestClient
+{
+    public partial class MetricsAPI
+    {
+        /// <summary>
+        /// Should be used to prevent memory leak in RetryPolicy
+        /// </summary>
+        public MetricsAPI(Uri baseUri, HttpClient client) : base(client)
+        {
+            Initialize();
+
+            BaseUri = baseUri ?? throw new ArgumentNullException("baseUri");
+        }
+    }
+}
+
+
